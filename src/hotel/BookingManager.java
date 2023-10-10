9ea3972e4abacbcf1e59ef7655130fbebf4f68ad
SonnyZ789123 <yoran.mertens@telenet.be>
2023-10-10 13:49:56 +0200

    Implement isRoomAvailable

--- src/hotel/BookingManager.java
+++ src/hotel/BookingManager.java
@@ -1,51 +1,60 @@
 package hotel;
 
 import java.time.LocalDate;
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
 
 public class BookingManager implements IBookingManager {
 
-	private Room[] rooms;
+    private Room[] rooms;
 
-	public BookingManager() {
-		this.rooms = initializeRooms();
-	}
+    public BookingManager() {
+        this.rooms = initializeRooms();
+    }
 
-	@Override
-	public Set<Integer> getAllRooms() {
-		Set<Integer> allRooms = new HashSet<Integer>();
-		Iterable<Room> roomIterator = Arrays.asList(rooms);
-		for (Room room : roomIterator) {
-			allRooms.add(room.getRoomNumber());
-		}
-		return allRooms;
-	}
+    @Override
+    public Set<Integer> getAllRooms() {
+        Set<Integer> allRooms = new HashSet<Integer>();
+        Iterable<Room> roomIterator = Arrays.asList(rooms);
+        for (Room room : roomIterator) {
+            allRooms.add(room.getRoomNumber());
+        }
+        return allRooms;
+    }
 
-	@Override
-	public boolean isRoomAvailable(Integer roomNumber, LocalDate date) {
-		//implement this method
-		return false;
-	}
+    @Override
+    public boolean isRoomAvailable(Integer roomNumber, LocalDate date) {
+        Room room = getRoom(roomNumber);
+        if (room == null) return false;
 
-	@Override
-	public void addBooking(BookingDetail bookingDetail) {
-		//implement this method
-	}
+        List<BookingDetail> bookingDetails = room.getBookings();
+        Optional<BookingDetail> bookingDetailOptional = bookingDetails.stream()
+                .filter(bookingDetail -> bookingDetail.getDate().equals(date)).findFirst();
+        return bookingDetailOptional.isEmpty();
+    }
 
-	@Override
-	public Set<Integer> getAvailableRooms(LocalDate date) {
-		//implement this method
-		return null;
-	}
+    @Override
+    public void addBooking(BookingDetail bookingDetail) {
+        //implement this method
+    }
 
-	private static Room[] initializeRooms() {
-		Room[] rooms = new Room[4];
-		rooms[0] = new Room(101);
-		rooms[1] = new Room(102);
-		rooms[2] = new Room(201);
-		rooms[3] = new Room(203);
-		return rooms;
-	}
+    @Override
+    public Set<Integer> getAvailableRooms(LocalDate date) {
+        //implement this method
+        return null;
+    }
+
+    private Room getRoom(Integer roomNumber) {
+        Optional<Room> roomOptional = Arrays.stream(rooms)
+                .filter(room -> room.getRoomNumber().equals(roomNumber)).findFirst();
+        return roomOptional.orElse(null);
+    }
+
+    private static Room[] initializeRooms() {
+        Room[] rooms = new Room[4];
+        rooms[0] = new Room(101);
+        rooms[1] = new Room(102);
+        rooms[2] = new Room(201);
+        rooms[3] = new Room(203);
+        return rooms;
+    }
 }
